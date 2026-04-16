0e3a3b70793a341ed3f5cb021120d638625415cf
Nils GB <nilsgrossebeikel@gmail.com>
2026-04-16 19:18:55 +0200

    Initial commit

---
+++ src/samples/map-sample/ol-app/services.ts
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
+// SPDX-License-Identifier: Apache-2.0
+import {
+    MapConfig,
+    MapConfigProvider,
+    MapConfigProviderOptions,
+    SimpleLayer
+} from "@open-pioneer/map";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+
+export const MAP_ID = "main";
+export class MainMapProvider implements MapConfigProvider {
+    mapId = MAP_ID;
+
+    async getMapConfig({ layerFactory }: MapConfigProviderOptions): Promise<MapConfig> {
+        return {
+            initialView: {
+                kind: "position",
+                center: { x: 847541, y: 6793584 },
+                zoom: 14
+            },
+            projection: "EPSG:3857",
+            layers: [
+                layerFactory.create({
+                    type: SimpleLayer,
+                    title: "OpenStreetMap",
+                    olLayer: new TileLayer({
+                        source: new OSM(),
+                        properties: { title: "OSM" }
+                    }),
+                    isBaseLayer: true
+                })
+            ]
+        };
+    }
+}
