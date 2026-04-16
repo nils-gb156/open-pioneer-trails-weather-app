57fac22869b1c8500746233a6a882b833783af13
Nils GB <nilsgrossebeikel@gmail.com>
2026-04-16 21:34:34 +0200

    feat: add Notifier

--- src/apps/weather-app/AppUI.tsx
+++ src/apps/weather-app/AppUI.tsx
@@ -1,12 +1,14 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 
+import { Notifier } from "@open-pioneer/notifier";
 import { Map } from "./components/Map";
 import { Header } from "./components/Header";
 
 export function AppUI() {
     return (
         <div style={{ display: "flex", flexDirection: "column", height: "100vh", width: "100%" }}>
+            <Notifier />
             <Header />
             <div style={{ flex: 1, overflow: "hidden" }}>
                 <Map />
