51b3efa75bc3471a692637dce5076543ef7dd611
Nils GB <nilsgrossebeikel@gmail.com>
2026-04-20 20:04:41 +0200

    remove unused imports

--- src/apps/weather-app/components/StaticCoordinateViewer.tsx
+++ src/apps/weather-app/components/StaticCoordinateViewer.tsx
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-import { Box, Text, Heading } from "@chakra-ui/react";
+import { Text, Heading } from "@chakra-ui/react";
 
 export interface StaticCoordinateViewerProps {
     coordinate?: [number, number];
